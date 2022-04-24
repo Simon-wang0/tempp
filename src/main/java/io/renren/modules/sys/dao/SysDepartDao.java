90da20c30fd53c84543cbf56e4e31b13763ad315
Simon-wang0 <1228204191@qq.com>
2022-04-24 15:03:42 +0800

    fix: same

--- src/main/java/io/renren/modules/sys/dao/SysDepartDao.java
+++ src/main/java/io/renren/modules/sys/dao/SysDepartDao.java
@@ -18,6 +18,6 @@ public interface SysDepartDao extends BaseMapper<SysDepartEntity> {
   void saveDepartAndRole(@Param("departId") long departId, @Param("roleId") Long roleIds);
   void updateDepartAndRole(@Param("departId") long departId, @Param("roleId") Long roleIds);
 
-  Long isSameDepart(@Param("name") String name,@Param("getDistinguishing") String getDistinguishing);
+  Long isSameDepart(@Param("name") String name,@Param("distinguishing") String distinguishing);
 
 }
